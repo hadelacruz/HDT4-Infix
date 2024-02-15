fbbd25f8f359441a6d84be38debfa5c252d80c22
jruiz002 <josegrg04@gmail.com>
2024-02-15 16:54:13 -0600

    Se agregó la lógica para los métodos push, pop y size en la lista encadenada.

--- StackList.java
+++ StackList.java
@@ -1,3 +1,57 @@
-public class StackList {
+public class StackList<T> extends AbstractStack<T>{
+
+    protected Node<T> lastNode;
+    protected Node<T> firstNode;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void push(T value) {
+        if (lastNode == null) {
+            lastNode = new Node<T>(value);
+            firstNode = lastNode;
+        } else {
+            Node<T> current = new Node<T>(value);
+            current.setNext(firstNode);
+            firstNode = current;
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public T pop() {
+        if (lastNode != null) {
+            if (firstNode.equals(lastNode)) {
+                T valueT = lastNode.getValue();
+                firstNode = null;
+                lastNode = null;
+                return valueT;
+            } else {
+                T valueFirst = firstNode.getValue();
+                firstNode = firstNode.getNext();
+                return valueFirst;
+            }
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Obtiene el tamaño actual de la pila.
+     * 
+     * @return El tamaño de la pila.
+     */
+    public int size() {
+        int count = 0;
+        Node<T> current = firstNode;
+        while (current != null) {
+            count++;
+            current = current.getNext();
+        }
+        return count;
+    }
     
 }
